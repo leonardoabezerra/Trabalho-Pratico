64a89f4eab1d59736a20b7d19ad99577df9f3d00
Renan Camara <151562116+renancamarafarias@users.noreply.github.com>
2023-12-19 12:20:37 -0300

    UI.java
    peguei os role do Cadastro do Paulo e coloquei na UI

--- trabalhoPratico/src/codigo/UI.java
+++ trabalhoPratico/src/codigo/UI.java
@@ -6,25 +6,25 @@ import codigo.modelos.Cliente;
 import codigo.modelos.OrdemServico;
 
 public class UI {
-        public static void main(String[] args) {
-            int opcao = 0;
-            do {
+         public static void main(String[] args) {
+        int opcao = 0;
+        do {
 
               // Obter dados do cliente
-            String nome = JOptionPane.showInputDialog("Informe o nome do cliente");
-            String contato = JOptionPane.showInputDialog("Informe o contato do cliente (email/telefone)");
-            String cpf = JOptionPane.showInputDialog("Informe o CPF do cliente");
+            String nome = JOptionPane.showInputDialog("Informe o nome do cliente: ");
+            String contato = JOptionPane.showInputDialog("Informe o contato do cliente (email/telefone): ");
+            String cpf = JOptionPane.showInputDialog("Informe o CPF do cliente: ");
 
             Cliente tempCliente = new Cliente(nome, contato, cpf);  // Referencia temporária para referenciar um cliente
-            Cadastro.cadastrarCliente(tempCliente);
+            cadastrarCliente(tempCliente);
 
               // Cadastrar veículo do cliente
-            Cliente.cadastrarVeiculo();
+            Cadastro.Cliente.cadastrarVeiculo();
 
               // Obter dados sobre a Ordem de Serviço
-            String mecanico = JOptionPane.showInputDialog("Informe o mecânico responsável pela ordem de serviço");
-            String dataEmissao = JOptionPane.showInputDialog("Informe a data de emissão da ordem de serviço");
-            String dataPrevista = JOptionPane.showInputDialog("Informe a data prevista para o final do serviço");
+            String mecanico = JOptionPane.showInputDialog("Informe o mecânico responsável pela ordem de serviço: ");
+            String dataEmissao = JOptionPane.showInputDialog("Informe a data de emissão da ordem de serviço: ");
+            String dataPrevista = JOptionPane.showInputDialog("Informe a data prevista para o final do serviço: ");
 
             OrdemServico tempOrdemServico = new OrdemServico(mecanico, dataEmissao, dataPrevista);  // Referencia temporaria para segurar dados da OS
             Cadastro.cadastrarOrdemServico(tempOrdemServico);
@@ -34,13 +34,13 @@ public class UI {
 
               // Verificar se o usuário deseja cadastrar mais uma OS
             opcao = JOptionPane.showConfirmDialog(null, "Deseja cadastrar mais uma ordem de serviço?");
-            
+
         } while (opcao == JOptionPane.YES_OPTION);  // Cadastrar nova OS caso o usuário selecione YES
 
           // Verificar se o usuário deseja ver os dados cadastrados
         int verDetalhes = JOptionPane.showConfirmDialog(null, "Deseja revisar os detalhes da(s) ordem de serviço(s) cadastrada(s)?");
         if (verDetalhes == JOptionPane.YES_OPTION) {
-            
+
               // Mostrar na tela os detalhes dos cadastros realizados
             for (int i = 0; i < cliente.length; i++) {
                 JOptionPane.showMessageDialog(null, "DADOS DA ORDEM DE SERVIÇO #" + (i+1));
@@ -57,4 +57,3 @@ public class UI {
 
         }
     }
-}
