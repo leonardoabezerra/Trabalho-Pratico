17c064c37d9514ffb3df5ed258ddd13f9766fd55
cainhas <67447767+cainhas@users.noreply.github.com>
2023-12-20 02:01:09 -0300

    Update Cliente.java

--- trabalhoPratico/src/codigo/modelos/Cliente.java
+++ trabalhoPratico/src/codigo/modelos/Cliente.java
@@ -1,37 +1,48 @@
 package codigo.modelos;
 
-public class Veiculo {
-      // Declaração de atributos
-    private String marca, cor, modelo;
-    private int anoModelo, anoFabricacao, combustivel;
-    private float quilometragem;
+import javax.swing.JOptionPane;
 
-      // Relatorio para mostrar informações do veículo para o usuário
+public class Cliente {
+      // Declaração de atributos
+    private String nome, contato, cpf;
+
+      // Construtor
+    public Cliente(String nome, String contato, String cpf) {
+        this.nome = nome;
+        this.contato = contato;  // email/telefone
+        this.cpf = cpf;
+    }
+
+      // Relatorio para mostrar dados do cliente ao usuário
     public String relatorio() {
         String resposta = "";
-        resposta += "**** DADOS DO VEÍCULO ****\n";
-        resposta += "MARCA: " + marca + "\n";
-        resposta += "COR: " + cor + "\n";
-        resposta += "MODELO: " + modelo + "\n";
-        resposta += "ANO DO MODELO: " + anoModelo + "\n";
-        resposta += "ANO DE FABRICAÇÃO: " + anoFabricacao + "\n";
-        resposta += "NÍVEL DE COMBUSTÍVEL: " + combustivel + "%\n";
-        resposta += "QUILOMETRAGEM ATUAL: " + quilometragem + "\n";
+        resposta += "**** DADOS DO CLIENTE ****\n"; 
+        resposta += "NOME: " + nome + "\n";
+        resposta += "CONTATO: " + contato + "\n";
+        resposta += "CPF: " + cpf + "\n";
 
         return resposta;
     }
 
-      // Construtor
-    public Veiculo(String marca, String cor, String modelo, int anoModelo, int anoFabricacao, int combustivel, float quilometragem) {
-        this.marca = marca;
-        this.cor = cor;
-        this.modelo = modelo;
-        this.anoModelo = anoModelo;
-        this.anoFabricacao = anoFabricacao;
-        this.combustivel = combustivel;
-        this.quilometragem = quilometragem;
+      // Método para realizar cadastro do veículo do cliente
+    static Veiculo[] veiculo = new Veiculo[0];
+
+    public static void cadastrarVeiculo(String marca, String cor, String modelo, int anoModelo, int anoFabricacao, int combustivel, float quilometragem) {
+        Veiculo tempVeiculo = new Veiculo(marca, cor, modelo, anoModelo, anoFabricacao, combustivel, quilometragem);  // Referencia temporaria para segurar os dados do veiculo
+
+          // Adicionar novo veiculo no array de veiculos
+        Veiculo tempV[] = new Veiculo[veiculo.length+1];
+        for (int i = 0; i < veiculo.length; i++) {
+        tempV[i] = veiculo[i];
+        }
+
+        tempV[veiculo.length] = tempVeiculo;
+        veiculo = tempV;
+
     }
 
-      
-    
+    public static String getVeiculo(int i) {
+        return veiculo[i].relatorio();
+    }
+
 }
