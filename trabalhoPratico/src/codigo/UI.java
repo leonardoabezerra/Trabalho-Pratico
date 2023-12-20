ba7d44ebe9778de5adb1effd097701bf9aaf8a57
cainhas <67447767+cainhas@users.noreply.github.com>
2023-12-20 01:57:56 -0300

    Update UI.java

--- trabalhoPratico/src/codigo/UI.java
+++ trabalhoPratico/src/codigo/UI.java
@@ -8,8 +8,10 @@ public class UI {
     public static void main(String[] args) {
         String[] opcoes = {"Adicionar Ordem de serviço", "Listar Dados","Finalizar"};
         int aux = 0;
+        
         while (true) {
             boolean sair = false;
+            
             int escolha = JOptionPane.showOptionDialog(null, "Selecione a tarefa desejada: ", "Bem vindo!",
             JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
 
@@ -38,32 +40,89 @@ public class UI {
 
                     // Se o usuário pressionar OK, obter os dados do cliente e cadastrar
                     if (result == JOptionPane.OK_OPTION) {
-                        String nome = nomeField.getText();
-                        String contato = contatoField.getText();
-                        String cpf = cpfField.getText();
-
+                            String nome = nomeField.getText();
+                            String contato = contatoField.getText();
+                            String cpf = cpfField.getText();
+                        
                         // Parte do leo
                         Cliente tempCliente = new Cliente(nome, contato, cpf);
                         Cadastro.cadastrarCliente(tempCliente);
 
+                        JPanel veiculoPanel = new JPanel();
+                        veiculoPanel.setLayout(new BoxLayout(veiculoPanel, BoxLayout.Y_AXIS));
+                          // Adicionar campos de entrada ao JPanel
+                        JTextField marcaField = new JTextField(10);
+                        JTextField modeloField = new JTextField(10);
+                        JTextField corField = new JTextField(10);
+                        JTextField anoMField = new JTextField(10);
+                        JTextField anoFField= new JTextField(10);
+                        JTextField combustivelField = new JTextField(10);
+                        JTextField quilometrageField = new JTextField(10);
 
-                        // Cadastrar veículo do cliente
-                        Cliente.cadastrarVeiculo();
+                        veiculoPanel.add(new JLabel("Marca do veículo:"));
+                        veiculoPanel.add(marcaField);
+                        veiculoPanel.add(new JLabel("Modelo:"));
+                        veiculoPanel.add(modeloField);
+                        veiculoPanel.add(new JLabel("Cor:"));
+                        veiculoPanel.add(corField);
+                        veiculoPanel.add(new JLabel("Ano do modelo:"));
+                        veiculoPanel.add(anoMField);
+                        veiculoPanel.add(new JLabel("Ano de Fabricação:"));
+                        veiculoPanel.add(anoFField);
+                        veiculoPanel.add(new JLabel("Combustivel em porcentagem (%):"));
+                        veiculoPanel.add(combustivelField);
+                        veiculoPanel.add(new JLabel("Quilometragem:"));
+                        veiculoPanel.add(quilometrageField);
+                        
+                            // Exibir o JPanel para entrada de dados do cliente
+                        int result2 = JOptionPane.showConfirmDialog(null, veiculoPanel, "Informe os dados do veículo", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+                        if (result2 == JOptionPane.OK_OPTION) {
+                            
+                            String marca = marcaField.getText();
+                            String modelo = modeloField.getText();
+                            String cor = corField.getText();
+                            int anoFabricacao = Integer.parseInt(anoFField.getText());
+                            int anoModelo = Integer.parseInt(anoMField.getText());
+                            int combustivel = Integer.parseInt(combustivelField.getText());
+                            float quilometragem = Float.parseFloat(quilometrageField.getText());
 
-                        // Obter dados sobre a Ordem de Serviço
-                        String mecanico = JOptionPane.showInputDialog("Informe o mecânico responsável pela ordem de serviço: ");
-                        String dataEmissao = JOptionPane.showInputDialog("Informe a data de emissão da ordem de serviço: ");
-                        String dataPrevista = JOptionPane.showInputDialog("Informe a data prevista para o final do serviço: ");
+                        Cliente.cadastrarVeiculo(marca, cor, modelo, anoModelo, anoFabricacao, combustivel, quilometragem);
 
-                        OrdemServico tempOrdemServico = new OrdemServico(mecanico, dataEmissao, dataPrevista);
-                        Cadastro.cadastrarOrdemServico(tempOrdemServico);
+                        JPanel OSPanel = new JPanel();
+                        OSPanel.setLayout(new BoxLayout(OSPanel, BoxLayout.Y_AXIS));
 
-                        // Iniciar cadastro dos itens
-                        Cadastro.ordemServico[aux++].cadastrarItens();
+                        // Adicionar campos de entrada ao JPanel
+                        JTextField mecanicoField = new JTextField(10);
+                        JTextField dataEField = new JTextField(10);
+                        JTextField dataPField = new JTextField(10);
+
+                        OSPanel.add(new JLabel("Mecanico Responsavel:"));
+                        OSPanel.add(mecanicoField);
+                        OSPanel.add(new JLabel("Data de Emissão:"));
+                        OSPanel.add(dataEField);
+                        OSPanel.add(new JLabel("Data Prevista"));
+                        OSPanel.add(dataPField);
+
+                        // Exibir o JPanel para entrada de dados do cliente
+                        int result3 = JOptionPane.showConfirmDialog(null, OSPanel, "Informe os dados da Ordem de Serviço", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+                        if (result3 == JOptionPane.OK_OPTION){
+                            String mecanico = mecanicoField.getText();
+                            String dataEmissao = dataEField.getText();
+                            String dataPrevista = dataPField.getText();
+                            
+                            OrdemServico tempOrdemServico = new OrdemServico(mecanico, dataEmissao, dataPrevista);
+
+                            Cadastro.cadastrarOrdemServico(tempOrdemServico);
+    
+                            // Iniciar cadastro dos itens
+                            Cadastro.ordemServico[aux++].cadastrarItens();          
+                            
+                        } 
+
+                    
                     }
-
                     break;
-
+                }
                 case 1:
                     // Verificar se o usuário deseja ver os dados cadastrados
                     // Opção 2
@@ -99,5 +158,5 @@ public class UI {
             break;
         }
     }
-}
+    }
 }
